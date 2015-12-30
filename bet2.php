9c5249be181582ac77060ac883e4d55a1e4524e7
lassayah <laurene.assayah@gmail.com>
2015-12-30 17:23:32 +0100

    bet 2 + nouvelle version base

--- bet2.php
+++ bet2.php
@@ -1,9 +1,16 @@
 <?php include('PHP/login.php'); ?>
 <?php include("head.php"); ?>
-<body id="page_bet2">
+<body id="page_bet1">
 <?php include("header.php"); ?>
 <?php include("menu.php"); ?>
 <?php include("PHP/bet_functions.php"); ?>
+<script src="javascript/jquery.min.js"></script>
+<script src="javascript/jquery-1.4.3.min.js"></script>
+<script src="fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
+<script src="fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+<link rel="stylesheet" href="fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+<script src="javascript/teams.js"></script>
+<script src="javascript/search.js"></script>
 
 <div id="content">
 <div class="mw1140p center">
@@ -24,66 +31,99 @@ else
 {
 ?>
 
-<h1 class="ptl">Pariez sur les Matchs Eliminatoires.</h1>
+<h1 class="ptl">Pariez sur les phases finales.</h1>
 <div id="search_team" class="mtl mbl"><label for="info_team">Rechercher une équipe&nbsp;:&nbsp;</label><input id="info_team" /></div>
 
-<div class="panel mbl">
+<div class="panel mbl" id="datematch">
 <h2>Accès direct</h2>
 
-<p><a href="#bet_huitiemes">Huitièmes de finale</a> <a href="#bet_quarts">Quarts de finale</a> <a href="#bet_demies">Demi-finales</a> <a href="#bet_petitefinale">Petite Finale</a> <a href="#bet_finale">Finale</a></p>
-<p><a href="#">Tableau de la phase finale sur le site de la FIFA</a></p>
+<div class="grid-3-small-1-tiny-1">
+<div>
+<p class="plm">1ers Matchs</p>
+<p><a href="#June10">Vendredi 10 juin</a> <a href="#June11">Samedi 11 juin</a> <a href="#June12">Dimanche 12 juin</a> <a href="#June13">Lundi 13 juin</a> <a href="#June14">Mardi 14 juin</a></p>
+</div>
+<div>
+<p class="plm">2èmes Matchs</p>
+<p> <a href="#June15">Mercredi 15 juin</a> <a href="#June16">Jeudi 16 juin</a> <a href="#June17">Vendredi 17 juin</a> <a href="#June18">Samedi 18 juin</a></p>
+</div>
+<div>
+<p class="plm">3èmes Matchs</p>
+<p> <a href="#June19">Dimanche 19 juin</a> <a href="#June20">Lundi 20 juin</a> <a href="#June21">Mardi 21 juin</a> <a href="#June22">Mercredi 22 juin</a></p>
+</div>
 
+</div><!--end grid-3-->
 </div><!--end Panel-->
 
-
-<div class="panel mbl">
-<h2>Les paris du jour</h2>
-<a href="#bet38">Angleterre - Espagne</a>
-</div><!--end Panel-->
-
+<?php 
+$daymatch = $bdd->query('SELECT t1.name AS tn1, t2.name AS tn2, HOUR(date) as hour, es.id AS sid 
+FROM euro_schedule es JOIN euro_team t1 ON team1 = t1.id 
+JOIN euro_team t2 ON t2.id = team2 WHERE DATE(date) LIKE CURDATE()');
+if ($daymatch->rowCount() > 0)
+{
+	echo "<div class=\"panel mbl\" id=\"daymatch\">
+<h2>Les paris du jour</h2>";
+while ($daymatchdata = $daymatch->fetch())
+{
+	echo "<a href=\"#bet" . $daymatchdata['sid'] . "\" >" . $daymatchdata['tn1'] . "/" . $daymatchdata['tn2'] . " - " . $daymatchdata['hour'] . "h </a>";
+}
+echo "</div>";
+}
+?>
 
 <!-- Loop to display all matches in chronological order with a select element to select the potential winning team -->
+<div class="grid-2-small-1-tiny-1" id="mainblock">
+<?php
+$bets = $bdd->prepare('SELECT t1.name AS tn1, t2.name AS tn2, t1.flag AS tf1, t2.flag as tf2, es.id as sid, team1result, team2result, available, 
+MONTHNAME(date) as month, DAY(date) as day, HOUR(date) as hour, MINUTE(date) as minute, win, available, t1.previous as previous1, t2.previous as previous2, 
+t1.smallname AS smallname1, t2.smallname AS smallname2, MONTHNAME(NOW()) as monthnow, DAY(NOW()) AS daynow, tempname1, tempname2 
+FROM euro_schedule es  LEFT OUTER JOIN euro_bet eb ON es.id = eb.match_id AND username = ? LEFT OUTER JOIN euro_team t1 ON team1 = t1.id 
+LEFT OUTER JOIN euro_team t2 ON t2.id = team2  WHERE es.group IS NULL ORDER BY date');
+$bets->execute(array($_SESSION['username']));
+while ($betsdata = $bets->fetch())
+{
+$team1 = $betsdata['tn1'] != NULL ? $betsdata['tn1'] : $betsdata['tempname1'];
+$team2 = $betsdata['tn2'] != NULL ? $betsdata['tn2'] : $betsdata['tempname2'];
+$teamresult1 = $betsdata['team1result'];
+$teamresult2 = $betsdata['team2result'];
+$id = $betsdata['sid'];
+$hasplayed = $teamresult1 != NULL && $teamresult2 != NULL;
+?>
 
-<h2 id="bet_huitiemes">Huitièmes de Finale</h2>
 
-<div class="grid-2-small-1-tiny-1">
 
-<div id="huitieme-1">
-<p class="h4-like pam"><span class="couleur">Samedi 25 juin</span> - 15h</p>
+
+<div class="bet-match-poule ptl pbl class1test">
+<p class="h4-like pam" id="<?php echo $betsdata['month'] . $betsdata['day']; ?>"><span class="couleur"><?php echo $betsdata['day']; if ($betsdata['month'] == "June") echo " Juin"; else if ($betsdata['day'] == "1" 
+&& $betsdata['month'] == "July") echo "er Juillet"; else echo " Juillet";
+	?></span> - <?php echo $betsdata['hour'] . ":";
+	if ($betsdata['minute'] < 10)
+		echo "0";
+	echo $betsdata['minute']; ?></p>
 <div class="panel txtcenter">
-<p class="mbs"><img src="images/flags/angleterre.png" alt="Angleterre" width="50" height="50"> <span class="h4-like medium-hidden small-hidden tiny-hidden mls mrs">Angleterre</span><span class="h4-like large-hidden mls mrs">ANG</span> vs <span class="h4-like medium-hidden small-hidden tiny-hidden mls mrs">Espagne</span><span class="h4-like large-hidden mls mrs">ESP</span> <img src="images/flags/espagne.png" alt="Espagne" width="50" height="50">
+<p class="ptl pbl"><img src="<?php echo $betsdata['tf1'];?>" alt="<?php echo $team1; ?>" width="50" height="50"> 
+<span class="h4-like medium-hidden small-hidden tiny-hidden mls mrs"><strong><?php echo $team1; ?></strong></span>
+<span class="h4-like large-hidden mls mrs"><strong><?php echo $betsdata['smallname1']; ?></strong></span> vs <span class="h4-like medium-hidden small-hidden tiny-hidden mls mrs"><strong><?php echo $team2; ?></strong></span>
+<span class="h4-like large-hidden mls mrs"><strong><?php echo $betsdata['smallname2']; ?></strong></span> <img src="<?php echo $betsdata['tf2'];?>" alt="<?php echo $team2; ?>" width="50" height="50">
 </p>
-<p class="txtcenter h2-like mtn"><strong>1 - 0</strong></p>
+<p class="txtcenter h5-like mtn"><strong>
+<?php
+if ($hasplayed)
+	echo $betsdata['team1result'] . " - " . $betsdata['team2result'];
+else
+	echo "non joué";
+?>
+</strong></p>
+<?php
+setLightbox($team1, $betsdata['previous1'], $betsdata['smallname1']);
+setLightbox($team2, $betsdata['previous2'], $betsdata['smallname2']);
+?>
 
-<a href="#lb_angleterre" id="lightbox_angleterre">Infos Angleterre</a>
-
-<!-- Setting the lightbox for each team available if we click on the name of the team -->
-		<div style="display: none;">
-		<div id="lb_angleterre" class="lbstyle">
-		<h2 class="h3-like">Angleterre</h2>
-			<p><strong>Palmarès 2011</strong><br>
-			Vainqueur</p>
-			<h3>Points Poule A</h3>
-			<ul class="unstyled pln">
-			<li>Australie&nbsp;:&nbsp;<strong>3</strong></li>
-			<li>Angleterre&nbsp;:&nbsp;<strong>1</strong></li>
-			<li>Pay de Galles&nbsp;:&nbsp;<strong>1</strong></li>
-			<li>Fidji&nbsp;:&nbsp;<strong>0</strong></li>
-			<li>Uruguay&nbsp;:&nbsp;<strong>0</strong></li>
-			</ul>
-			<h3>Phase finale</h3>
-			<ul class="unstyled pln">
-			<li><strong>quarts</strong>&nbsp;:&nbsp;non joué</li>
-			<li><strong>demies</strong>&nbsp;:&nbsp;non joué</li>
-			<li><strong>finale</strong>&nbsp;:&nbsp;non joué</li>
-			</ul>
-		</div>
-		</div>
 		<!-- End Setting the lightbox for each team available if we click on the name of the team -->
-
- - <a href="#lb_espagne" id="lightbox_espagne">Infos Espagne</a>
+ 
 
 <!-- Setting the lightbox for each team available if we click on the name of the team -->
+<!--
+ <a href="#lb_espagne" id="lightbox_espagne">Infos Roumanie</a>
 		<div style="display: none;">
 		<div id="lb_espagne" class="lbstyle">
 		<h2 class="h3-like">Espagne</h2>
@@ -111,122 +151,78 @@ else
 			</ul>
 		</div>
 		</div>
+		-->
 		<!-- End Setting the lightbox for each team available if we click on the name of the team -->
 
-<div class="grid-2-small-1-tiny-1">
+<div class="grid-2-small-1-tiny-1 ptl">
 <div>
-<form id="bet37" action="bet2.php#bet37" method="POST" class="border-bet-form">
+<form id="bet<?php echo $id; ?>" action="bet1.php#bet<?php echo $id; ?>" method="POST" class="border-bet-form">
 <ul class="unstyled pln bet-choix txtleft">
-			<li><label for="Angleterre"><input type="radio" value="Angleterre" name="select_bet37">&nbsp;Angleterre</label></li>
-			<li><label for="Espagne"><input type="radio" value="Espagne" name="select_bet37">&nbsp;Espagne</label></li>
+			<li><label for="<?php echo $team1; ?>"><input type="radio" value="<?php echo $team1; ?>" name="select_bet<?php echo $id; ?>">&nbsp;<?php echo $team1; ?></label></li>
+			<li><label for="<?php echo $team2; ?>"><input type="radio" value="<?php echo $team2; ?>" name="select_bet<?php echo $id; ?>">&nbsp;<?php echo $team2; ?></label></li>
+			<li><label for="Nul"><input type="radio" name="select_bet1" value="Nul">&nbsp;Nul</label></li>
+			<?php
+				if ($betsdata['available'] == "1") {
+			?>
+			<li class="mtm"><input type="submit" class="btn-rouge mts mbs" name="save_bet<?php echo $id; ?>" id="save_bet<?php echo $id; ?>" value="Valider"></li>
+			<?php } else { ?>
 			<li class="pam"><strong>Paris fermés</strong></li>
+			<?php } ?>
 </ul></form>
 </div>
 <div class="pam">
-<p>Votre choix</p>
-<p class="couleur">Angleterre</p>
-<p class="mtm">Résultat</p>
-<p class="couleur">Gagné</p>
-<p>Perdu</p>
+<p class="big pbn">Votre choix</p>
+<p class="couleur mtn"><?php 
+if ($betsdata['win'] != "")
+      echo $betsdata['win'];
+    else
+      echo "Non Parié";
+?></p>
+<p class="mtm big pbn">Résultat</p>
+<?php
+if ($teamresult1 != NULL && $teamresult2 != NULL)
+{
+	if ($teamresult1 > $teamresult2)
+		$winningteam = $team1;
+	else if ($teamresult2 > $teamresult1)
+		$winningteam= $team2;
+	else
+	{
+		if ($teamresult1 == $teamresult2 && $betsdata['group'] == null)
+		{
+			if ($betsdata['team1penalty'] > $betsdata['team2penalty'])
+				$winningteam = $team1;
+			else
+				$winningteam = $team2;
+				
+		}
+		else
+			$winningteam = "Nul";
+	}
+	if ($winningteam == $betsdata['win'])
+		echo "<p class=\"couleur mtn\">Gagné</p>";
+	else
+		echo "<p class=\"couleur mtn\">Perdu</p>";
+}
+else
+{
+	echo "<p class=\"couleur mtn\">Match non joué</p>";
+}
+?>
 </div>
 </div><!--end grid-2-->
 </div><!--end panel-->
-</div><!--end huitieme-1-->
-		
-
-<div id="huitieme-2">
-<p class="h4-like pam"><span class="couleur">Samedi 25 juin</span> - 18h</p>
-<div class="panel txtcenter">
-<p class="mbs"><img src="images/flags/angleterre.png" alt="Angleterre" width="50" height="50"> <span class="h4-like medium-hidden small-hidden tiny-hidden mls mrs">Angleterre</span><span class="h4-like large-hidden mls mrs">ANG</span> vs <span class="h4-like medium-hidden small-hidden tiny-hidden mls mrs">Espagne</span><span class="h4-like large-hidden mls mrs">ESP</span> <img src="images/flags/espagne.png" alt="Espagne" width="50" height="50">
-</p>
-
-<p class="txtcenter h2-like mtn"><strong>non joué</strong></p>
-
-<a href="#lb_angleterre" id="lightbox_angleterre">Infos Angleterre</a>
-
-<!-- Setting the lightbox for each team available if we click on the name of the team -->
-		<div style="display: none;">
-		<div id="lb_angleterre" class="lbstyle">
-		<h2 class="h3-like">Angleterre</h2>
-			<p><strong>Palmarès 2011</strong><br>
-			Vainqueur</p>
-			<h3>Points Poule A</h3>
-			<ul class="unstyled pln">
-			<li>Australie&nbsp;:&nbsp;<strong>3</strong></li>
-			<li>Angleterre&nbsp;:&nbsp;<strong>1</strong></li>
-			<li>Pay de Galles&nbsp;:&nbsp;<strong>1</strong></li>
-			<li>Fidji&nbsp;:&nbsp;<strong>0</strong></li>
-			<li>Uruguay&nbsp;:&nbsp;<strong>0</strong></li>
-			</ul>
-			<h3>Phase finale</h3>
-			<ul class="unstyled pln">
-			<li><strong>quarts</strong>&nbsp;:&nbsp;non joué</li>
-			<li><strong>demies</strong>&nbsp;:&nbsp;non joué</li>
-			<li><strong>finale</strong>&nbsp;:&nbsp;non joué</li>
-			</ul>
-		</div>
-		</div>
-		<!-- End Setting the lightbox for each team available if we click on the name of the team -->
-
- - <a href="#lb_espagne" id="lightbox_espagne">Infos Espagne</a>
-
-<!-- Setting the lightbox for each team available if we click on the name of the team -->
-		<div style="display: none;">
-		<div id="lb_espagne" class="lbstyle">
-		<h2 class="h3-like">Espagne</h2>
-			<p><strong>Palmarès Euro 2012 : </strong><br>
-			non qualifié</p>
-			<h3>Matchs de Poule</h3>
-			<ul class="unstyled pln">
-			<li><strong>1er</strong> vs France&nbsp;:&nbsp;perdu 3 - 0</li>
-			<li><strong>2ème</strong> vs Belgique&nbsp;:&nbsp;non joué</li>
-			<li><strong>3ème</strong> vs Ukraine&nbsp;:&nbsp;non joué</li>
-			</ul>
-			<h3>Points Groupe A</h3>
-			<ul class="unstyled pln">
-			<li>France&nbsp;:&nbsp;<strong>3</strong></li>
-			<li>Ukraine&nbsp;:&nbsp;<strong>1</strong></li>
-			<li>Belgique&nbsp;:&nbsp;<strong>1</strong></li>
-			<li>Malte&nbsp;:&nbsp;<strong>0</strong></li>
-			</ul>
-			<h3>Phase finale</h3>
-			<ul class="unstyled pln">
-			<li><strong>8èmes</strong>&nbsp;:&nbsp;non joué</li>
-			<li><strong>quarts</strong>&nbsp;:&nbsp;non joué</li>
-			<li><strong>demies</strong>&nbsp;:&nbsp;non joué</li>
-			<li><strong>finale</strong>&nbsp;:&nbsp;non joué</li>
-			</ul>
-		</div>
-		</div>
-		<!-- End Setting the lightbox for each team available if we click on the name of the team -->
-
-<div class="grid-2-small-1-tiny-1">
-<div>
-<form id="bet38" action="bet2.php#bet38" method="POST" class="border-bet-form">
-<ul class="unstyled pln bet-choix txtleft">
-			<li><label for="Angleterre"><input type="radio" value="Angleterre" name="select_bet38">&nbsp;Angleterre</label></li>
-			<li><label for="Espagne"><input type="radio" value="Espagne" name="select_bet38">&nbsp;Espagne</label></li>
-			<li class="mtm"><input type="submit" class="btn-rouge mts mbs" name="save_bet38" id="save_bet38" value="Valider"></li>
-</ul></form>
-</div>
-<div class="pam">
-<p>Votre choix</p>
-<p class="couleur">Angleterre</p>
-<p class="mtm">Résultat</p>
-<p class="couleur">Non joué</p>
-</div>
-</div><!--end grid-2-->
-</div><!--end panel-->
-</div><!--end huitieme-2-->
-		
-
-</div><!--end grid-2-->
+</div><!--end poule ang esp-->
+<?php
+}
+?>
+<!--</div>--><!--end grid-2-->
 
 
 <p class="up"><a class="icon-up" href="#container">&nbsp;Haut de page</a></p>
 </div><!--end center-->
 </div><!--end content-->
-<?php }?>
+<?php } ?>
 <?php include("footer.php"); ?>
 </div><!--end#container-->
 </body>
