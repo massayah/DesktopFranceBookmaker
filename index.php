ba58732e211e445843c67e3a3c596845bfd6c323
massayah <massayah@creasites.fr>
2016-03-11 20:55:27 +0100

    accueil carte stades

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <div class="mw1140p center">
 <div id="intro" class="mtl mbl">
 <h1>It's Free, It's Fun, It's Competition</h1>
+<img src="images/StadesMap.png" alt="Stades" width="360" height="369" class="fr" />
 <p>Après la coupe du monde 2014, place à l'Euro 2016.</p>
 <p>La compétition se déroulera du 10 juin au 10 juillet 2016.</p>
 <p>24 équipes - 1 champion</p>
@@ -89,10 +90,22 @@
 <p>Laurène a créé tout le code interactif du site.</p>
 <p>Martine a créé le design et le code de mise en page du site, et a installé et personnalisé le CMS WordPress avec extension BuddyPress pour la plate-forme Discussion.</p>
 <p class="up"><a href="#container">
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 60">
-  <circle cx="30" cy="30" r="25" class="circle" />
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 60" width="60" height="60">
+<defs>
+    <filter id="ombre" y="-10" height="40" x="-10" width="150">
+        <feOffset in="SourceAlpha" dx="1" dy="1" result="offset2" />
+        <feGaussianBlur in="offset2" stdDeviation="3"  result="blur2"/>
+
+        <feMerge>
+            <feMergeNode in="blur2" />
+            <feMergeNode in="SourceGraphic" />
+        </feMerge>
+    </filter>
+</defs>
+  <circle cx="30" cy="30" r="25" class="circle" style="filter: url(#ombre);" />
   <polyline points="18,37 30,20 42,37" class="arrow" />
   </svg></a></p>
+  
 </div><!--end center-->
 </div><!--end content-->
 <?php include("footer.php"); ?>
